2716ee53fb73fbf1f327ca82720e3fe6807102f5
Bill Wareham <bwareham24@hotmail.com>
2013-05-13 03:43:11 +0000

    added franchise record to prior view

--- season/views.py
+++ season/views.py
@@ -1,8 +1,10 @@
 from django.template import Context, loader
 from season.models import Season
+from gamemaker.models import Game
 from section.models import Page, Item
 from django.http import HttpResponse
 from django.utils import timezone
+from django.db.models import Sum, F
 
 def prior(request):
     #latest_games_list = Game.objects.filter(DateTime__gte=timezone.now()).order_by('DateTime')
@@ -11,12 +13,25 @@ def prior(request):
     column_counts = count/4
     home_pk = Page.objects.get(header='Prior Seasons').pk
     content_list = Item.objects.filter(page = home_pk, published = True).order_by('position')
+    played_games_list = Game.objects.filter(DateTime__lte=timezone.now()).order_by('-DateTime')
+    wins = played_games_list.filter(scoreMPR__gt=F('scoreOPP')).count()
+    losses = played_games_list.filter(scoreOPP__gt=F('scoreMPR')).count()
+    ties = wins = played_games_list.filter(scoreMPR__exact=F('scoreOPP')).count()
+    pastWins = Season.objects.aggregate(Sum('wins'))
+    pastLosses = Season.objects.aggregate(Sum('losses'))
+    pastTies = Season.objects.aggregate(Sum('ties'))
     t = loader.get_template('season/prior.html')
     c = Context({
         'seasons': seasons,
         'count': count,
         'column_counts': column_counts,
         'content_list': content_list,
+        'wins': wins,
+        'losses': losses,
+        'ties': ties,
+        'pastWins': pastWins,
+        'pastLosses': pastLosses,
+        'pastTies': pastTies,
     })
     return HttpResponse(t.render(c))
 
@@ -32,3 +47,4 @@ def season_detail(request, season):
         'roster': roster,
     })
     return HttpResponse(t.render(c))
+
